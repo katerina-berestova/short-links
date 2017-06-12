72d18c455b9864523d5357133d912accc1b08e9b
Ekaterina Berestova <ekaterina.berestova@noveogroup.com>
2017-06-12 21:28:16 +0700

    Generating and saving of short link have been added

--- app/index.js
+++ app/index.js
@@ -1,5 +1,37 @@
 "use strict";
 
-module.exports = function(app) {
+var urlModel = require('./model/url');
+var config = require('./../config');
 
+var responseCodes = {
+    httpOK: 200,
+    httpCreated: 201,
+    httpBadRequest: 400,
+    httpError: 500,
+};
+
+
+module.exports = function(app) {
+    app.listen(config.port, function(){
+        console.log('Server is listening on port 8000');
+    });
+
+    app.get('/', function (req, res) {
+        res.status(responseCodes.httpOK).send('API is running');
+    });
+
+    app.post('/', function(req, res) {
+        if (req.body !== undefined && req.body.url !== undefined) {
+            urlModel.save(req.body.url).then(function (code) {
+                res.status(responseCodes.httpCreated).send(config.host + '/' + code);
+                return;
+            }, function (reason) {
+                res.status(responseCodes.httpError).send('Internal error');
+                return;
+            });
+
+        } else {
+            res.status(responseCodes.httpBadRequest).send('Request is invalid');
+        }
+    });
 };
--- app/model/url.js
+++ app/model/url.js
@@ -1,1 +1,129 @@
 "use strict";
+
+var config = require('../../config');
+var mysql = require('mysql');
+var sha1 = require('sha1');
+
+var connection = mysql.createConnection({
+    'host': config.db.host,
+    'user': config.db.user,
+    'password' : config.db.password,
+    'database' : config.db.dbname
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
+
+
+function getRandomInt(min, max) {
+    return Math.random() * (max - min) + min;
+};
+
+
+function insertRow(url, code) {
+
+    console.log(url, mysql.escape(url));
+
+    return new Promise(function(resolve, reject) {
+        var row = {
+            'url': url,
+            'code': code
+        };
+        var query = connection.query('INSERT INTO link SET ?', row, function (error, results) {
+            if (error) {
+                reject(error)
+            };
+            resolve(row);
+        });
+    });
+};
+
+function getRowByUrl(url) {
+    return new Promise(function(resolve, reject) {
+        var query = connection.query('SELECT code FROM link WHERE `url` = ?', url , function (error, results) {
+            if (error) {
+                reject(error);
+                return;
+            }
+            if (results[0] !== undefined) {
+                resolve(results[0]);
+                return;
+            }
+            resolve(null);
+            return;
+        });
+    });
+};
+
+function getRowByCode(code) {
+
+    return new Promise(function(resolve, reject) {
+        var query = connection.query('SELECT url FROM link WHERE `code` = ?', code, function (error, results) {
+            if (error) {
+                reject(error);
+                return;
+            }
+            if (results[0] !== undefined) {
+                resolve(results[0]);
+                return;
+            }
+            resolve(null);
+            return;
+
+        });
+    });
+};
+
+function generateUniqueCode() {
+
+    return new Promise(function(resolve, reject) {
+        function generate() {
+            var length = 6;
+            var hash = sha1(new Date().getTime());
+            var start = getRandomInt(0, 34);
+            var code = hash.substring(start, start+length).toUpperCase();
+
+            getRowByCode(code).then(function(result) {
+                if (result !== null) {
+                    generate();
+                }
+                resolve(code);
+                return;
+            }).catch(function (error) {
+                reject(error);
+                return;
+            })
+        }
+        generate();
+    });
+};
+
+module.exports = {
+    save: function (url) {
+        return new Promise(function (resolve, reject) {
+            getRowByUrl(url).then(function (row) {
+                if (!row) {
+                    generateUniqueCode().then(function (code) {
+                        return insertRow(url, code);
+                    }).then(function (row) {
+                        resolve(row.code);
+                        return;
+                    });
+                } else {
+                    resolve(row.code);
+                    return;
+                }
+            }).catch(function (error) {
+                reject(error);
+                return;
+            });
+        });
+    },
+};
