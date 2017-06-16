a22a8f21588852193c6d80bc6bce83a8938dde0e
eberestova <Ekaterina.Berestova@noveogroup.com>
2017-06-16 19:59:55 +0700

    Code has been refactored

--- app/model/url.js
+++ app/model/url.js
@@ -2,86 +2,73 @@
 
 const config = require('../../config');
 const mysql = require('mysql');
-const sha1 = require('sha1');
 const {host, user, database, password} = config.db;
+const codegenerator = require('../helper/codegenerator');
 const connection = mysql.createConnection({ host, user, database, password });
+
+module.exports = { save, getUrlByCode };
 
 connection.connect(err => {
   if (err)
     return console.error('error connecting: ' + err.stack);
-  return console.log('connected as id ' + connection.threadId);
+  return console.debug('connected as id ' + connection.threadId);
 });
-
-function getRandomInt(min, max) {
-  return Math.random() * (max - min) + min;
-};
 
 function insertRow(url, code) {
   return new Promise((resolve, reject) => {
     const row = { url, code };
-    connection.query('INSERT INTO link SET ?', row, (error, results) => {
+    connection.query('INSERT INTO `link` SET ?', row, (error, results) => {
       if (error) return reject(error);
       resolve(row);
     });
   });
-};
+}
 
-function getRowByUrl(url) {
-  return new Promise((resolve, reject) => {
-    connection.query('SELECT code FROM link WHERE `url` = ?', url , (error, results) => {
-      if (error) return reject(error);
-      resolve(results[0]);
+function getRow(params) {
+    return new Promise((resolve, reject) => {
+        var field = Object.keys(params.where)[0],
+            value = params.where[field];
+        var sql = `SELECT ${params.select} FROM ${params.from} WHERE ${field} = ?`;
+        connection.query(sql, `${value}`, (error, results) => {
+            if (error) return reject(error);
+            resolve(results[0]);
+        });
     });
-  });
-};
+}
 
-function getRowByCode(code) {
-  return new Promise((resolve, reject) => {
-    connection.query('SELECT url FROM link WHERE `code` = ?', code, (error, results) => {
-      if (error) return reject(error);
-      resolve(results[0]);
-    });
-  });
-};
-
-function generateUniqueCode() {
+function createCode() {
   return new Promise((resolve, reject) => {
     function generate() {
-      const length = 6;
-      var hash = sha1(new Date().getTime());
-      var start = getRandomInt(0, 34);
-      var code = hash.substring(start, start+length).toUpperCase();
-      getRowByCode(code).then(result => {
+      var code = codegenerator.generateRandomCode();
+      getRow({ select: 'url', from: 'link', where: { code: code } }).then(result => {
         if (!result) generate();
         resolve(code);
       }).catch(error => reject(error));
     }
     generate();
   });
-};
+}
 
 function save(url) {
   return new Promise((resolve, reject) => {
-    getRowByUrl(url).then(row => {
+    getRow({ select: 'code', from: 'link', where: { url: url } }).then(row => {
       if (!row) {
-        generateUniqueCode().then(code => {
+        createCode().then(code => {
           return insertRow(url, code)
         }).then(row => resolve(row.code));
       } else
       resolve(row.code);
     }).catch(error => reject(error));
   });
-};
+}
 
 function getUrlByCode(code) {
   return new Promise((resolve, reject) => {
-    getRowByCode(code).then(row => {
-      if (!row)
-        resolve(null);
-      else
-        resolve(row.url);
+    getRow({ select: 'url', from: 'link', where: { code: code } }).then(row => {
+    if (!row)
+      resolve(null);
+    else
+      resolve(row.url);
     }).catch(error => reject(error));
   });
-};
-
-module.exports = { save, getUrlByCode };
+}
--- server.js
+++ server.js
@@ -4,4 +4,53 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-require('./app')(app);
+const validUrl = require('valid-url');
+const urlModel = require('./app/model/url');
+const config = require('./config');
+const { host, port } = config;
+
+const responseCodes = {
+    httpOK: 200,
+    httpCreated: 201,
+    httpBadRequest: 400,
+    httpNotFound: 404,
+    httpError: 500,
+};
+
+function cropLastSlash(str) {
+  return str.slice(-1) === '/' ? str.slice(0, -1) : str;
+}
+
+app.listen(port, () => console.log(`Server is listening on port  ${port}`));
+
+app.get('/', (req, res) => {
+  res.status(responseCodes.httpOK).send('API is running');
+});
+
+app.get('/:code', (req, res) => {
+  urlModel.getUrlByCode(req.params.code).then(url => {
+  if (url) return res.redirect(url);
+    res.status(responseCodes.httpNotFound).send('Url for this code haven\'t been found on the server');
+  }, reason => {
+    console.debug(reason);
+    return res.status(responseCodes.httpError).send('Internal server error');
+  });
+});
+
+app.post('/', (req, res) => {
+  var url = req.body && req.body.url;
+
+  if (!validUrl.isHttpUri(req.body.url, true)) {
+    res.status(responseCodes.httpBadRequest).send('Request is invalid');
+  }
+
+  url = cropLastSlash(url);
+
+  urlModel.save(url).then(
+    code => res.status(responseCodes.httpCreated).send(`${host}:${port}/${code}`),
+    reason => {
+        console.debug(reason);
+      res.status(responseCodes.httpError).send('Internal server error');
+    }
+  );
+});
