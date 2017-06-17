13faece13d1a99c3d2b9fc7cd7e388b9f504ed36
Ekaterina Berestova <ekaterina.berestova@noveogroup.com>
2017-06-17 22:37:05 +0700

    Some functions has been renamed, code has been changed according ES6

--- app/helper/codegenerator.js
+++ app/helper/codegenerator.js
@@ -1,16 +1,16 @@
 "use strict";
 
 const sha1 = require('sha1');
+const length = 6;
 
-module.exports = { generateRandomCode };
+module.exports = { create };
 
 function getRandomInt(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
-function generateRandomCode() {
-    const length = 6;
-    var hash = sha1(new Date().getTime());
-    var start = getRandomInt(0, 34);
-    return hash.substring(start, start + length).toUpperCase();
+function create() {
+  const hash = sha1(new Date().getTime());
+  const start = getRandomInt(0, 34);
+  return hash.substring(start, start + length).toUpperCase();
 }
--- app/model/url.js
+++ app/model/url.js
@@ -1,49 +1,54 @@
 "use strict";
 
-const config = require('../../config');
 const mysql = require('mysql');
-const {host, user, database, password} = config.db;
 const codegenerator = require('../helper/codegenerator');
+const config = require('../../config');
+const { host, user, database, password } = config.db;
+
 const connection = mysql.createConnection({ host, user, database, password });
 
 module.exports = { save, getUrlByCode };
 
 connection.connect(err => {
   if (err)
-    return console.error('error connecting: ' + err.stack);
-  return console.debug('connected as id ' + connection.threadId);
+    return debug('error connecting: ' + err.stack);
+  console.log('connected as id ' + connection.threadId);
 });
 
-function insertRow(url, code) {
+function insert(url, code) {
   return new Promise((resolve, reject) => {
     const row = { url, code };
-    connection.query('INSERT INTO `link` SET ?', row, (error, results) => {
+
+    connection.query('INSERT INTO `link` SET ?', row, error => {
       if (error) return reject(error);
       resolve(row);
     });
   });
 }
 
-function getRow(params) {
-    return new Promise((resolve, reject) => {
-        var field = Object.keys(params.where)[0],
-            value = params.where[field];
-        var sql = `SELECT ${params.select} FROM ${params.from} WHERE ${field} = ?`;
-        connection.query(sql, `${value}`, (error, results) => {
-            if (error) return reject(error);
-            resolve(results[0]);
-        });
+function get(params) {
+  return new Promise((resolve, reject) => {
+    const { select, from, where } = params;
+    const field = Object.keys(where);
+    const value = where[field];
+
+    const sql = `SELECT ${select} FROM ${from} WHERE ${field} = ?`;
+
+    connection.query(sql, `${value}`, (error, results) => {
+      if (error) return reject(error);
+      resolve(results[0]);
     });
+  });
 }
 
 function createCode() {
   return new Promise((resolve, reject) => {
     function generate() {
-      var code = codegenerator.generateRandomCode();
-      getRow({ select: 'url', from: 'link', where: { code: code } }).then(result => {
+      var code = codegenerator.create();
+      get({ select: 'url', from: 'link', where: { code } }).then(result => {
         if (!result) generate();
         resolve(code);
-      }).catch(error => reject(error));
+      }).catch(reject);
     }
     generate();
   });
@@ -51,24 +56,23 @@ function createCode() {
 
 function save(url) {
   return new Promise((resolve, reject) => {
-    getRow({ select: 'code', from: 'link', where: { url: url } }).then(row => {
+    get({ select: 'code', from: 'link', where: { url } }).then(row => {
       if (!row) {
         createCode().then(code => {
-          return insertRow(url, code)
+          return insert(url, code);
         }).then(row => resolve(row.code));
       } else
       resolve(row.code);
-    }).catch(error => reject(error));
+    }).catch(reject);
   });
 }
 
 function getUrlByCode(code) {
   return new Promise((resolve, reject) => {
-    getRow({ select: 'url', from: 'link', where: { code: code } }).then(row => {
-    if (!row)
-      resolve(null);
-    else
-      resolve(row.url);
-    }).catch(error => reject(error));
+    const options = { select: 'url', from: 'link', where: { code } };
+
+    get(options)
+      .then(row => resolve(row && row.url))
+      .catch(reject);
   });
 }
